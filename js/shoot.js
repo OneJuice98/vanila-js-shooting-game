964c6412d31d953e9f0dce4d1b135c5c3dc73b93
‍원정수[학생](전자정보대학 전자공학과) <waterpurifier@khu.ac.kr>
2022-07-09 15:00:00 +0000

    refactoring, shooting

--- js/shoot.js
+++ js/shoot.js
@@ -1,6 +1,7 @@
 const bullet = {
     value : -1,
     display : "●",
+    speed : 500,
 }
 // 잔상제거를 위한 함수! 작동원리는 display와 동일!
 
@@ -10,19 +11,22 @@ function displayBullet(event){
         // 변수를 따로 빼는 이유는 tank의 좌표를 건들까봐
         const bulletX = tank.x;
         // 위로만 향하기에 얘만 let
-        let bulletY = tank.y;
+        let bulletY = tank.y - Math.floor(tank.height)+1;
         const tr = document.querySelectorAll('tr');
+        let count = 0;
         for(let i=bulletY; i>0; i--){
             const td = tr[i].querySelectorAll('td');
-            td[bulletX].innerText = bullet.display;
+            
             setTimeout(function(){
+                td[bulletX].innerText = bullet.display;
 				const lastTd = tr[i+1].querySelectorAll('td');
             	lastTd[bulletX].innerText = SPACE_VALUE;
             
-            }, 500);
-    		
+            }, bullet.speed * count);
+    		count += 1;
         }
     }
 }    
+
 
 document.addEventListener("keydown", displayBullet);
